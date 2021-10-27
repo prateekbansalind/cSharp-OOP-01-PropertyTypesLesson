6254ed5c60a2998373179e0c02e6e5c23886e24b
pbansal16 <prateekbansalind@gmail.com>
2021-10-27 15:04:56 +1300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel("Bansal");
+
+            person.FirstName = "Prateek";
+            //person.LastName = "Bansal";
+            person.Age = 40;
+            person.SSN = "123-45-6789";
+
+            Console.WriteLine(person.FullName);
+            Console.WriteLine(person.SSN);
+
+            Console.ReadLine();
+        }
+    }
+}
